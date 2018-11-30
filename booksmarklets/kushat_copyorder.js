6df4bc2813fcde80b4b4117b983e33fdae1c55bf
raziel <x-alexander93@yandex.ru>
2018-11-30 19:00:02 +0200

    added kushat_copyorder.js and booksmarklets folder

---
+++ booksmarklets/kushat_copyorder.js
@@ -0,0 +1,48 @@
+var items = document.getElementsByClassName('item_cell');
+
+var [dayOfWeek, date] = document.getElementsByClassName('today_day')[0].innerText.split("\n");
+
+var orderItems = '';
+for (var item of items) {
+    orderItems += item.getElementsByClassName('title')[0].firstChild.innerText + "\n"
+}
+
+var order = `На ${dayOfWeek} (${date}):\n${orderItems}`;
+
+copyTextToClipboard(order);
+
+function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+
+    textArea.style.padding = 0;
+
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+
+    document.body.removeChild(textArea);
+}

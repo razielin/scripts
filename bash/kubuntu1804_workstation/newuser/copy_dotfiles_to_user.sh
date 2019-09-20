8b2ecbfb64f6123aaba1334def0b06c34529cd79
raziel <x-alexander93@yandex.ru>
2019-09-20 15:44:13 +0300

    added disable kwallet keyring

--- bash/kubuntu1804_workstation/newuser/copy_dotfiles_to_user.sh
+++ bash/kubuntu1804_workstation/newuser/copy_dotfiles_to_user.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )" # script dir
 
-#unzip "$DIR/dotfiles"
+if [[ ! -d "$DIR/dotfiles" ]]; then
+    unzip "$DIR/dotfiles"
+fi
 cp -r "$DIR/dotfiles/." ~
-#rm "$DIR/dotfiles"bi011irid
+#rm -fr "$DIR/dotfiles"
+
+# disable kwallet keyring
+cd ~/.local/share/keyrings
+rm -fr ./*
+cd $(kde4-config --localprefix)share/apps/kwallet
+rm -fr ./*
